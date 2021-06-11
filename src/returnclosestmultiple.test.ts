1f6ee3a3de5e96713530715fc5d939be34dcf0d1
Yunas A <yatteyeh@gmail.com>
2021-06-11 17:59:55 +0300

    3rd

--- src/returnclosestmultiple.test.ts
+++ src/returnclosestmultiple.test.ts
@@ -2,5 +2,6 @@ import returnClosestMultiple10 from "./returnclosestmultiple";
 
 test("returns closest multiple of 10", () => {
   expect(returnClosestMultiple10(19)).toBe(20);
-  expect(returnClosestMultiple10(4)).toBe(10);
+  expect(returnClosestMultiple10(4)).toBe(0);
+  expect(returnClosestMultiple10(453)).toBe(450);
 });
