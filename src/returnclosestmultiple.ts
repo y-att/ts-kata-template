e18c57bf03a54638d8e67fcada21d85c92b79376
Yunas A <yatteyeh@gmail.com>
2021-06-11 17:57:13 +0300

    2nd

--- src/returnclosestmultiple.ts
+++ src/returnclosestmultiple.ts
@@ -17,14 +17,18 @@ Expected output:
 //Given a number return the closest number to it that is divisible by 10.
 
 //pseudocode
-// function returnClosestMultiple10 (number) 
-function returnClosestMultiple10 (a : number): number {
+// function returnClosestMultiple10 (number)
+function returnClosestMultiple10(a: number): number {
+  //if (a < 5 || a[0] < 5)
 
-    //if (a < 5 || a[0] < 5)
-
-    let closestMultiple : number = (a + 5)/10 * 10;  // closest integer to `a` that is divisible by `b`
-
-    return closestMultiple 
+  //let closestMultiple : number = (a + 5)/10 * 10;  // closest integer to `a` that is divisible by `b`
+  let c1: number = a - (a % 10);
+  let c2: number = a + 10 - (a % 10);
+  if (a - c1 > c2 - a) {
+    return c2;
+  } else {
+    return c1;
+  }
 }
 
 export default returnClosestMultiple10;
@@ -34,6 +38,6 @@ export default returnClosestMultiple10;
 // if number - or second digit is less than 5- round down nearest multiple
 // if number- or second digit is greater than=> 5- round up
 // closestMultiple = number
-// return closestMultiple 
+// return closestMultiple
 
 // example returnClosestMultiple10 (6)
